1a43c65f7367bbf8cbb6ec616d859bada8e900b4
darkmud <sean.cheng012345@gmail.com>
2022-02-22 20:20:46 -0500

    Server, Client barebones

--- server.c
+++ server.c
@@ -3,16 +3,26 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+
+#include <sys/types.h>   //===================
+#include <sys/socket.h>  // For getaddrinfo()
+#include <netinet/in.h>  //
+#include <netdb.h>       //===================
+
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
 
+int getaddrinfo(const char *node,                  // e.g "www.example.com" OR IP addr
+		const char *service,               // e.g "http", "ftp", or port num
+		const struct addrinfo *node_info,  // struct addrinfo of relevant info
+		struct addrinfo **res);            // Linked List of Results
+
 int main(int argc, char* argv[]) {
-	printf("test");
-	
+
+	int status;
+	struct addrinfo node_info;
+	struct addrinfo *server_info;
+
 	return 0;
 }
